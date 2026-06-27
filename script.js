2239a38a3762d968d44a1b7d6d5f52604a6b8404
oleksandrbilyi212 <oleksandrbilyi99@gmail.com>
2026-06-27 12:47:33 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+// Плавна поява блоків під час прокрутки
+
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.style.opacity = "1";
+            entry.target.style.transform = "translateY(0)";
+        }
+    });
+},{
+    threshold:0.2
+});
+
+sections.forEach(section=>{
+
+    section.style.opacity="0";
+    section.style.transform="translateY(50px)";
+    section.style.transition="0.8s";
+
+    observer.observe(section);
+
+});
+
+
+// Поточний рік у футері
+
+const footer = document.querySelector("footer p");
+
+footer.innerHTML = `© ${new Date().getFullYear()} Oleksandr Bilyi`;
